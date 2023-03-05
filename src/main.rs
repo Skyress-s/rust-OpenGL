a8b5e54702f4956623c4b469f8c8b0ea5c66183f
Mathias M <mathmoe64@gmail.com>
2023-03-05 02:39:00 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,227 @@
+﻿
+extern crate glfw;
+mod Shader;
+
+use cgmath::num_traits::ToPrimitive;
+use cgmath::SquareMatrix;
+use glfw::{Action, Context, Key, WindowEvent};
+
+const SCREEN_WIDTH : u32 = 1200u32;
+const SCREEN_HEIGHT : u32 = 600u32;
+
+fn main() {
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    // glfw::WindowHint(gl::PROFILE)
+
+    // Create a windowed mode window and its OpenGL context
+    let (mut window, events) = glfw.create_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    // Make the window's context current
+    window.make_current();
+    window.set_key_polling(true);
+
+    // setup callback
+    window.set_size_polling(true);
+
+    // loading opengl function pointers
+    gl::load_with(|s| glfw.get_proc_address_raw(s));
+    gl::Viewport::load_with(|s| glfw.get_proc_address_raw(s));
+
+    // user
+
+    type Vertex = [f32; 3];
+
+    const VERTICES: [Vertex; 6] =
+    [[-0.5, -0.5, 0.0], [0.5, -0.5, 0.0], [0.0, 0.5, 0.0],
+        [-0.5, -0.0, 0.0], [0.5, -0.0, 0.0], [0.0, 1.0, 0.0]];
+
+
+    unsafe {
+        // load_gl_with(|f_name| win.get_proc_address(f_name));
+        gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+
+        let mut vao = 0;
+        gl::GenVertexArrays(1, &mut vao);
+        assert_ne!(vao, 0);
+        gl::BindVertexArray(vao);
+
+        let mut vbo = 0;
+        gl::GenBuffers(1, &mut vbo);
+        assert_ne!(vbo, 0);
+
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        // glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            core::mem::size_of_val(&VERTICES) as isize,
+            VERTICES.as_ptr().cast(),
+            gl::STATIC_DRAW,
+        );
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            core::mem::size_of::<Vertex>().try_into().unwrap(),
+            0 as *const _,
+        );
+        gl::EnableVertexAttribArray(0);
+    }
+
+    let mainShader = Shader::Shader::build(&"vertex.txt".to_string(), &"fragment.txt".to_string());
+    mainShader.use_shader();
+    let mut model = cgmath::Matrix4::from_value(1f32);
+    mainShader.set_mat4x4_String("model".to_string(), model);
+    mainShader.set_mat4x4_String("view".to_string(), model);
+    mainShader.set_mat4x4_String("projection".to_string(), model);
+
+    // Loop until the user closes the window
+    'mainLoop: while !window.should_close() {
+        // Swap front and back buffers
+        window.swap_buffers();
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::ClearColor(1.0,1.0,1.0,1.0);
+        }
+        // Poll for and process events
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            handle_glfw_events(&event, & mut window);
+            println!("{:?}", event);
+        }
+        model = cgmath::Matrix4::from_translation(cgmath::Vector3::new(0.0001f32,0f32,0f32)) * model;
+        mainShader.set_mat4x4_String("model".to_string(), model);
+
+        unsafe {
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+    }
+}
+
+fn handle_glfw_events(events : &WindowEvent,  window : &mut glfw::Window){
+    match events {
+        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+            window.set_should_close(true)
+        },
+        glfw::WindowEvent::Key(Key::W, _, Action::Press, _) => {
+            window.set_size(SCREEN_WIDTH.to_i32().expect(""), (SCREEN_HEIGHT + 20u32).to_i32().expect(""))
+        },
+        glfw::WindowEvent::Size(x,y) => unsafe {
+            gl::Viewport(0,0,*x,*y); // TODO mabye dereferencing isnt the best idea here?
+            println!("{0} : {1}", x, y)
+        },
+        glfw::WindowEvent::ContentScale(x,y) =>{
+            println!("{}", "YEAH")
+        },
+
+        _ => {},
+    }
+}
+
+// mod shader;
+//
+// extern crate glutin;
+// extern crate gl;
+//
+// use cgmath::{Matrix4, SquareMatrix, Vector3};
+// use glutin::event::{Event, VirtualKeyCode, WindowEvent};
+// use glutin::event_loop::{ControlFlow, EventLoop};
+// use glutin::window::WindowBuilder;
+// use glutin::ContextBuilder;
+// use gl::types::*;
+// use crate::shader::Shader;
+//
+// fn main() {
+//     // creates the context and version
+//     let event_loop = EventLoop::new();
+//     let window_builder = WindowBuilder::new()
+//         .with_title("OpenGL Window")
+//         .with_inner_size(glutin::dpi::LogicalSize::new(800.0, 600.0));
+//     let context_builder = ContextBuilder::new();
+//     let windowed_context = context_builder
+//         .build_windowed(window_builder, &event_loop)
+//         .unwrap();
+//
+//     let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+//     gl::load_with(|symbol| windowed_context.get_proc_address(symbol) as *const _);
+//     unsafe {
+//         gl::Viewport(0, 0, 800, 600);
+//     }
+//     // user setup
+//
+//     type Vertex = [f32; 3];
+//
+//     const VERTICES: [Vertex; 6] =
+//     [[-0.5, -0.5, 0.0], [0.5, -0.5, 0.0], [0.0, 0.5, 0.0],
+//         [-0.5, -0.0, 0.0], [0.5, -0.0, 0.0], [0.0, 1.0, 0.0]];
+//
+//
+//     unsafe {
+//         // load_gl_with(|f_name| win.get_proc_address(f_name));
+//         gl::ClearColor(0.2, 0.3, 0.3, 1.0);
+//
+//         let mut vao = 0;
+//         gl::GenVertexArrays(1, &mut vao);
+//         assert_ne!(vao, 0);
+//         gl::BindVertexArray(vao);
+//
+//         let mut vbo = 0;
+//         gl::GenBuffers(1, &mut vbo);
+//         assert_ne!(vbo, 0);
+//
+//         gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+//         // glBindBuffer(GL_ARRAY_BUFFER, vbo);
+//
+//         gl::BufferData(
+//             gl::ARRAY_BUFFER,
+//             core::mem::size_of_val(&VERTICES) as isize,
+//             VERTICES.as_ptr().cast(),
+//             gl::STATIC_DRAW,
+//         );
+//         gl::VertexAttribPointer(
+//             0,
+//             3,
+//             gl::FLOAT,
+//             gl::FALSE,
+//             core::mem::size_of::<Vertex>().try_into().unwrap(),
+//             0 as *const _,
+//         );
+//         gl::EnableVertexAttribArray(0);
+//     }
+//
+//     let mainShader = Shader::build(&"vertex.txt".to_string(), &"fragment.txt".to_string());
+//     mainShader.use_shader();
+//     let mut model = cgmath::Matrix4::from_value(1f32);
+//     mainShader.set_mat4x4_String("u_matrix".to_string(), model);
+//
+//     // Render loop
+//
+//     event_loop.run(move |event, _, control_flow| {
+//         // polling and events
+//         *control_flow = ControlFlow::Wait;
+//         match event {
+//             Event::WindowEvent { event, .. } => match event {
+//                 WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+//                 WindowEvent::KeyboardInput { input, .. } => {
+//                     if let Some(VirtualKeyCode::Escape) = input.virtual_keycode {
+//                         *control_flow = ControlFlow::Exit;
+//                     }
+//                 }
+//                 _ => (),
+//             },
+//             Event::LoopDestroyed => return,
+//             _ => (),
+//         }
+//         unsafe { gl::Clear(gl::COLOR_BUFFER_BIT) }
+//
+//         model = Matrix4::from_translation(Vector3::new(0.0001f32,0f32,0f32)) * model;
+//         mainShader.set_mat4x4_String("u_matrix".to_string(), model);
+//         unsafe {
+//             gl::DrawArrays(gl::TRIANGLES, 0, 3);
+//         }
+//
+//         windowed_context.swap_buffers().unwrap();
+//     });
+// }
---
+++ src/shader.rs
@@ -0,0 +1,150 @@
+use std;
+use std::ffi::{CStr, CString};
+use std::fmt::{Display, Formatter};
+use std::fs;
+use std::io::BufRead;
+use cgmath::Matrix;
+use gl;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Shader {
+    shader_program: gl::types::GLuint,
+}
+
+#[derive(Debug)]
+enum ShaderType{
+    Vertex, Geometry, Fragment
+}
+
+
+impl Shader {
+    fn check_shader_compile_errors(id: gl::types::GLuint, shader_type: ShaderType, file_path: &String) {
+        unsafe {
+            let mut success = 0;
+            gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success);
+            if success == 0 {
+                let mut v: Vec<u8> = Vec::with_capacity(1024);
+                let mut log_len = 0_i32;
+                gl::GetShaderInfoLog(
+                    id,
+                    1024,
+                    &mut log_len,
+                    v.as_mut_ptr().cast(),
+                );
+                v.set_len(log_len.try_into().unwrap());
+                panic!("{0:?} Compile Error: {1}", shader_type, String::from_utf8_lossy(&v));
+            }
+        }
+    }
+
+    fn check_program_compile_errors(id: gl::types::GLuint){
+        let mut success = 0;
+        unsafe {
+            gl::GetProgramiv(id, gl::LINK_STATUS, &mut success);
+            if success == 0 {
+                let mut v: Vec<u8> = Vec::with_capacity(1024);
+                let mut log_len = 0_i32;
+                gl::GetProgramInfoLog(
+                    id,
+                    1024,
+                    &mut log_len,
+                    v.as_mut_ptr().cast(),
+                );
+                v.set_len(log_len.try_into().unwrap());
+                panic!("Program Link Error: {}", String::from_utf8_lossy(&v));
+            }
+        }
+    }
+
+    // constructor
+    pub fn build(vertex_file_path: &String, fragment_file_path: &String) -> Self {
+        let mut inst = Self {
+            shader_program: 0
+        };
+
+        let vert_shader_string = fs::read_to_string(vertex_file_path).expect(
+            "Should have been able to read file");
+        // println!("{}", vert_shader_string);
+        let frag_shader_string = fs::read_to_string(fragment_file_path).expect(
+            "Should have been able to read file");
+        // println!("{}", frag_shader_string);
+
+        unsafe {
+
+            // VERTEX SHADER
+            let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+            assert_ne!(vertex_shader, 0);
+            gl::ShaderSource(
+                vertex_shader,
+                1,
+                &(vert_shader_string.as_bytes().as_ptr().cast()),
+                &(vert_shader_string.len().try_into().unwrap()),
+            );
+            gl::CompileShader(vertex_shader);
+            Self::check_shader_compile_errors(vertex_shader, ShaderType::Vertex, vertex_file_path);
+
+            // FRAGMENT SHADER
+            let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+            assert_ne!(fragment_shader, 0);
+            gl::ShaderSource(
+                fragment_shader,
+                1,
+                &(frag_shader_string.as_bytes().as_ptr().cast()),
+                &(frag_shader_string.len().try_into().unwrap()),
+            );
+            gl::CompileShader(fragment_shader);
+            Self::check_shader_compile_errors(fragment_shader, ShaderType::Fragment, fragment_file_path);
+
+            // PROGRAM
+            inst.shader_program = gl::CreateProgram();
+            // let shader_program = glCreateProgram();
+            assert_ne!(inst.shader_program, 0);
+            gl::AttachShader(inst.shader_program, vertex_shader);
+            gl::AttachShader(inst.shader_program, fragment_shader);
+            gl::LinkProgram(inst.shader_program);
+            Self::check_program_compile_errors(inst.shader_program);
+
+            gl::DeleteShader(vertex_shader);
+            gl::DeleteShader(fragment_shader);
+
+            // Self.use_shader();
+            // glUseProgram(inst.shader_program);
+        }
+        inst
+    }
+
+    pub fn print_info(&self){
+        println!("{}", "test!");
+    }
+}
+
+
+impl Shader {
+    pub fn use_shader(&self){
+        unsafe{
+            gl::UseProgram(self.shader_program);
+        }
+    }
+
+    pub fn set_mat4x4(&self,name : CString, mat4x4: cgmath::Matrix4<f32>){
+        unsafe{
+            let location = gl::GetUniformLocation(self.shader_program, name.as_bytes_with_nul().as_ptr() as *const i8);
+            gl::UniformMatrix4fv(location, 1, gl::FALSE, mat4x4.as_ptr() as *const f32)
+        }
+    }
+
+    pub fn set_mat4x4_String(&self, name : String, mat4x4 : cgmath::Matrix4<f32>){
+        let name2 = CString::new(name).expect("CString::Failed");
+        Self::set_mat4x4(&self, name2, mat4x4);
+        // let location = glGetUniformLocation(self.shader_program, name.as_bytes_with_nul().as_ptr() as *const i8);
+        // glUniformMatrix4fv(location, 1, GL_FALSE, mat4x4.as_ptr() as *const f32)
+    }
+}
+
+impl Drop for Shader{
+    fn drop(&mut self) {
+        unsafe{
+            gl::DeleteProgram(self.shader_program);
+        }
+    }
+}
